392084114e549162ca0f8ba8b429989b5dbb57ff
sefacelik <sefacelik1800@gmail.com>
2020-05-14 01:44:49 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Sefa Çelik on 14.05.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
